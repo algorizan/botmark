7432ab25555565a1b2af9d49ab38a52d527bf761
Izan <cuetarai@myumanitoba.ca>
2022-07-07 18:16:14 -0500

    Created functions to look up, add, and delete servers from the database. Modified database-test.js to test the new functions.

--- db/database-access.js
+++ db/database-access.js
@@ -1,6 +1,6 @@
 /**
  * @author: Izan Cuetara Diez (a.k.a. Unstavle)
- * @version: v1.1 | 2022-07-06
+ * @version: v1.1 | 2022-07-07
  */
 
 "use strict";
@@ -9,28 +9,76 @@ const pg = require('pg');
 const { getClient } = require('./database-connection');
 
 module.exports = {
-	async selectServer(botId) {
+	async selectServer(botId, serverId) {
+		let result = false;
 		const client = await getClient();
 
 		if (client instanceof pg.Client) {
-			const query = `
-				SELECT serverid, name FROM servers
-				WHERE botid = $1
-				;
-			`;
-			const entries = await client.query(query, [botId]);
+			try {
+				const query = `
+					SELECT serverid, name FROM servers
+					WHERE botid = $1 AND serverid = $2
+					;
+				`;
+				const entries = await client.query(query, [ botId, serverId ]);
+				// console.log(Object.keys(entries.rows?.[0]).join('\t'));
+				// console.log(`${entries.rows.map((r) => Object.values(r).join('\t')).join('\n')}`);
 
-			console.log(Object.keys(entries.rows?.[0]).join('\t'));
-			console.log(`${entries.rows.map((r) => Object.values(r).join('\t')).join('\n')}`);
+				result = entries;
+			}
+			catch (error) {
+				console.error(`Error selecting serverId ${serverId}\n${error}`);
+			}
 
 			await client.end();
 		}
 		else {
 			console.log('Invalid database client.');
 		}
+		return result;
 	},
 
-	// async insert(botId, table) {
+	async insertServer(botId, serverId, serverName) {
+		let result = false;
+		const client = await getClient();
 
-	// },
+		if (client instanceof pg.Client) {
+			try {
+				const query = `INSERT INTO servers(serverid, name, botid) VALUES ($2, $3, $1);`;
+				await client.query(query, [ botId, serverId, serverName ]);
+				console.log(`Inserted server "${serverName}"`);
+				result = true;
+			}
+			catch (error) {
+				console.error(`Error inserting serverId ${serverId}\n${error}`);
+			}
+			await client.end();
+		}
+		else {
+			console.log('Invalid database client.');
+		}
+		return result;
+	},
+
+	async removeServer(botId, serverId) {
+		let result = false;
+		const client = await getClient();
+
+		if (client instanceof pg.Client) {
+			try {
+				const query = `DELETE FROM servers WHERE botid = $1 AND serverid = $2;`;
+				await client.query(query, [ botId, serverId ]);
+				console.log(`Removed server with id "${serverId}"`);
+				result = true;
+			}
+			catch (error) {
+				console.log(`Error deleting serverId ${serverId}\n${error}`);
+			}
+			await client.end();
+		}
+		else {
+			console.log('Invalid database client.');
+		}
+		return result;
+	},
 };
--- db/database-test.js
+++ db/database-test.js
@@ -1,30 +1,16 @@
 /**
  * @author: Izan Cuetara Diez (a.k.a. Unstavle)
- * @version: v1.1 | 2022-07-06
+ * @version: v1.1 | 2022-07-07
  */
 
 "use strict";
 
-const pg = require('pg');
-const { getClient } = require('./database-connection');
+const db = require('./database-access');
 
 (async () => {
-	const client = await getClient();
+	const botid = '911410913138081863';
+	const homeServer = '528382510455848963';
 
-	if (client instanceof pg.Client) {
-		const botid = '911410913138081863';
-		const query = `
-			SELECT * FROM servers
-			WHERE botid = $1
-			;
-		`;
-		const entries = await client.query(query, [botid]);
-		console.log(Object.keys(entries.rows?.[0]).join('\t'));
-		console.log(`${entries.rows.map((r) => Object.values(r).join('\t')).join('\n')}`);
-
-		await client.end();
-	}
-	else {
-		console.log('Invalid database client.');
-	}
+	const entries = await db.selectServer(botid, homeServer);
+	console.log(entries.rows[0]?.name + '\n' + entries.rows[0]?.serverid);
 })();
